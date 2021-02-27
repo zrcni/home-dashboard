5ed0331dacce58c5759dab5cae6bd7466636ff10
zrcni <zrcni9@gmail.com>
2021-02-27 21:01:03 +0200

    log app start

--- prod/app/core.clj
+++ prod/app/core.clj
@@ -2,10 +2,13 @@
   (:require [app.renderer :as renderer]
             [app.mqtt :as mqtt]
             [app.temperature-mode.mqtt :as temperature-mode-mqtt]
-            [app.shutdown :as shutdown])
+            [app.shutdown :as shutdown]
+            [app.logger :as log])
   (:gen-class))
 
 (defn -main [& _args]
+  (log/info "Starting the app!")
+
   (shutdown/add-hook :mqtt/disconnect mqtt/disconnect!)
   (shutdown/add-hook :renderer/unmount renderer/unmount!)
   (shutdown/add-hook :clojure.core/shutdown-agents shutdown-agents)
