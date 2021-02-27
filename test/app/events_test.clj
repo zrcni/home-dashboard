2c6304a47ace3729ec38a7b358b4aa3e369c9abf
zrcni <zrcni9@gmail.com>
2021-02-27 12:57:39 +0200

    updated tests

--- test/app/events_test.clj
+++ test/app/events_test.clj
@@ -1,62 +1,52 @@
 (ns app.events-test
   (:require [clojure.test :refer :all]
-            [app.events.api :as events-api]
-            [app.events.handlers :as handlers]
-            [app.state :as state]
-            [test-utils :refer [wait-until]]))
-
-(defn init-events [*state]
-  (let [*channels (atom {})
-        dispatch #(events-api/dispatch *channels %)
-        subscribe #(events-api/subscribe *channels *state %1 %2)]
-    [dispatch subscribe]))
+            [app.state.default-state :refer [default-state]]
+            [app.state.factory :refer [create-context]]
+            [app.cljfx-utils :refer [ctx-state]]
+            [test-utils :refer [wait-until create-dispatcher]]))
 
 (deftest show-menu
   (testing "show menu"
-    (let [*state (atom state/default-state)
-          [dispatch subscribe] (init-events *state)]
-      (handlers/register subscribe)
+    (let [*context (atom (create-context default-state))
+          dispatch (create-dispatcher {:context *context})]
       (dispatch :show-menu)
-      (wait-until (:menu? @*state)))))
+      (wait-until (:menu? (ctx-state @*context))))))
 
 (deftest hide-menu
   (testing "hide menu"
-        (let [*state (atom state/default-state)
-              [dispatch subscribe] (init-events *state)]
-          (handlers/register subscribe)
+        (let [*context (atom (create-context default-state))
+              dispatch (create-dispatcher {:context *context})]
           (dispatch :show-menu)
           (dispatch :hide-menu)
-          (wait-until (not (:menu? @*state))))))
+          (wait-until (not (:menu? (ctx-state @*context)))))))
 
 (deftest change-mode-static-image
   (testing "change active mode to static image"
-    (let [*state (atom (assoc state/default-state :active-mode :wolfenstein))
-          [dispatch subscribe] (init-events *state)]
-      (handlers/register subscribe)
+    (let [*context (atom (create-context (assoc default-state
+                                                :active-mode :wolfenstein)))
+          dispatch (create-dispatcher {:context *context})]
       (dispatch :activate-mode-static-image)
-      (wait-until (= (:active-mode @*state) :static-image)))))
+      (wait-until (= (:active-mode (ctx-state @*context)) :static-image)))))
 
 (deftest change-mode-wolfenstein
   (testing "change active mode to wolfenstein"
-    (let [*state (atom (assoc state/default-state :active-mode :static-image))
-          [dispatch subscribe] (init-events *state)]
-      (handlers/register subscribe)
+    (let [*context (atom (create-context (assoc default-state
+                                                :active-mode :static-image)))
+          dispatch (create-dispatcher {:context *context})]
       (dispatch :activate-mode-wolfenstein)
-      (wait-until (= (:active-mode @*state) :wolfenstein)))))
+      (wait-until (= (:active-mode (ctx-state @*context)) :wolfenstein)))))
 
 (deftest change-mode-hide-menu
   (testing "changing active mode hides menu"
-    (let [*state (atom state/default-state)
-          [dispatch subscribe] (init-events *state)]
-      (handlers/register subscribe)
+    (let [*context (atom (create-context default-state))
+          dispatch (create-dispatcher {:context *context})]
       (dispatch :show-menu)
       (dispatch :activate-mode-static-image)
-      (wait-until (not (:menu? @*state))))))
+      (wait-until (not (:menu? (ctx-state @*context)))))))
 
 (deftest change-mode-already-selected-hide-menu
   (testing "changing active mode to already selected only hides the menu"
-    (let [*state (atom state/default-state)
-          [dispatch subscribe] (init-events *state)]
-      (handlers/register subscribe)
+    (let [*context (atom (create-context default-state))
+          dispatch (create-dispatcher {:context *context})]
       (dispatch :activate-mode-static-image)
-      (wait-until (not (:menu? @*state))))))
+      (wait-until (not (:menu? (ctx-state @*context)))))))
--- test/test_utils.clj
+++ test/test_utils.clj
@@ -1,5 +1,16 @@
 (ns test-utils
-  (:import java.time.Instant))
+  (:import java.time.Instant)
+  (:require [app.events.factory
+             :refer [create-dispatcher]
+             :rename {create-dispatcher create-dispatcher-original}]
+            [app.events.effects :as effects]))
+
+(defn create-dispatcher
+  "Add custom effects by default"
+  [desc]
+  (create-dispatcher-original (merge {:effects {:activate-mode-wolfenstein! effects/activate-mode-wolfenstein!
+                                                :deactivate-mode-wolfenstein! effects/deactivate-mode-wolfenstein!}}
+                                     desc)))
 
 (defn now-ms []
   (.toEpochMilli (Instant/now)))
