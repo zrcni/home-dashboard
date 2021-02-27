ce5c33c087d1f4262b566836d1fa3673dea5610d
zrcni <zrcni9@gmail.com>
2021-02-27 13:33:13 +0200

    added metosin/testit for async test assertions updated tests to use metosin/testit

--- test/app/events_test.clj
+++ test/app/events_test.clj
@@ -1,52 +1,55 @@
 (ns app.events-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest]]
+            [testit.core :refer [fact =eventually=>]]
             [app.state.default-state :refer [default-state]]
             [app.state.factory :refer [create-context]]
             [app.cljfx-utils :refer [ctx-state]]
-            [test-utils :refer [wait-until create-dispatcher]]))
+            [test-utils :refer [create-dispatcher]]))
 
 (deftest show-menu
-  (testing "show menu"
-    (let [*context (atom (create-context default-state))
-          dispatch (create-dispatcher {:context *context})]
-      (dispatch :show-menu)
-      (wait-until (:menu? (ctx-state @*context))))))
+  (let [*context (atom (create-context default-state))
+        dispatch (create-dispatcher {:context *context})]
+    (dispatch :show-menu)
+
+    (fact "menu is opened"
+     (:menu? (ctx-state @*context)) =eventually=> true)))
+
 
 (deftest hide-menu
-  (testing "hide menu"
-        (let [*context (atom (create-context default-state))
-              dispatch (create-dispatcher {:context *context})]
-          (dispatch :show-menu)
-          (dispatch :hide-menu)
-          (wait-until (not (:menu? (ctx-state @*context)))))))
+  (let [*context (atom (create-context default-state))
+        dispatch (create-dispatcher {:context *context})]
+    (dispatch :show-menu)
+    (dispatch :hide-menu)
+    (fact "menu is closed"
+          (:menu? (ctx-state @*context)) =eventually=> false)))
 
-(deftest change-mode-static-image
-  (testing "change active mode to static image"
-    (let [*context (atom (create-context (assoc default-state
-                                                :active-mode :wolfenstein)))
-          dispatch (create-dispatcher {:context *context})]
-      (dispatch :activate-mode-static-image)
-      (wait-until (= (:active-mode (ctx-state @*context)) :static-image)))))
+(deftest change-mode-static-image 
+  (let [*context (atom (create-context (assoc default-state
+                                              :active-mode :wolfenstein)))
+        dispatch (create-dispatcher {:context *context})]
+    (dispatch :activate-mode-static-image)
+    (fact "active mode is changed to :static-image"
+     (:active-mode (ctx-state @*context)) =eventually=> :static-image)))
 
 (deftest change-mode-wolfenstein
-  (testing "change active mode to wolfenstein"
-    (let [*context (atom (create-context (assoc default-state
-                                                :active-mode :static-image)))
-          dispatch (create-dispatcher {:context *context})]
-      (dispatch :activate-mode-wolfenstein)
-      (wait-until (= (:active-mode (ctx-state @*context)) :wolfenstein)))))
+  (let [*context (atom (create-context (assoc default-state
+                                              :active-mode :static-image)))
+        dispatch (create-dispatcher {:context *context})]
+    (dispatch :activate-mode-wolfenstein)
+    (fact "active mode is changed to :wolfenstein"
+     (:active-mode (ctx-state @*context)) =eventually=> :wolfenstein)))
 
 (deftest change-mode-hide-menu
-  (testing "changing active mode hides menu"
-    (let [*context (atom (create-context default-state))
-          dispatch (create-dispatcher {:context *context})]
-      (dispatch :show-menu)
-      (dispatch :activate-mode-static-image)
-      (wait-until (not (:menu? (ctx-state @*context)))))))
+  (let [*context (atom (create-context default-state))
+        dispatch (create-dispatcher {:context *context})]
+    (dispatch :show-menu)
+    (dispatch :activate-mode-static-image)
+    (fact "changing mode hides the menu"
+     (:menu? (ctx-state @*context)) =eventually=> false)))
 
 (deftest change-mode-already-selected-hide-menu
-  (testing "changing active mode to already selected only hides the menu"
-    (let [*context (atom (create-context default-state))
-          dispatch (create-dispatcher {:context *context})]
-      (dispatch :activate-mode-static-image)
-      (wait-until (not (:menu? (ctx-state @*context)))))))
+  (let [*context (atom (create-context default-state))
+        dispatch (create-dispatcher {:context *context})]
+    (dispatch :activate-mode-static-image)
+    (fact "activating already active mode hides the menu"
+          (:menu? (ctx-state @*context)) =eventually=> false)))
