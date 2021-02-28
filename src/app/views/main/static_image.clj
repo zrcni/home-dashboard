6c3b094fef900a636e19249584ec90c2356324d9
zrcni <zrcni9@gmail.com>
2021-02-28 21:48:48 +0200

    show image thumbnail list's scrollbar when cursor is hidden

--- src/app/views/main/static_image.clj
+++ src/app/views/main/static_image.clj
@@ -1,6 +1,7 @@
 (ns app.views.main.static-image
   (:require [cljfx.api :as fx]
             [app.subs :as subs]
+            [app.config :refer [cfg]]
             [app.events.api :refer [create-event]]))
 
 (defn image [{:keys [image]}]
@@ -20,19 +21,20 @@
              :image image}})
 
 (defn thumbnail-list [{:keys [images]}]
-{:fx/type :scroll-pane
- :style-class "thumbnail-scroll-pane"
- :fit-to-width true
- :hbar-policy :never
- :vbar-policy :never
- :content {:fx/type :v-box
-           :alignment :center
-           :children (mapv
-                      (fn [image]
-                        {:fx/type thumbnail-button
-                         :image image
-                         :on-action (create-event :image-mode-select-image {:image image})})
-                      images)}})
+  {:fx/type :scroll-pane
+   :style-class "thumbnail-scroll-pane"
+   :fit-to-width true
+  ;; Scroll bar must be shown, because scrolling with touch doesn't work ATM.
+   :vbar-policy (if (:cursor? cfg) :never :always)
+   :hbar-policy :never
+   :content {:fx/type :v-box
+             :alignment :center
+             :children (mapv
+                        (fn [image]
+                          {:fx/type thumbnail-button
+                           :image image
+                           :on-action (create-event :image-mode-select-image {:image image})})
+                        images)}})
 
 (defn static-image-view [{:keys [fx/context]}]
   (let [mode (fx/sub-ctx context subs/static-image-mode)]
