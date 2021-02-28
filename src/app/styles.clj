794cf4d5609f0467ce52698d37e640a67844e421
zrcni <zrcni9@gmail.com>
2021-02-28 22:08:34 +0200

    styled image thumbnail scrollbar

--- src/app/styles.clj
+++ src/app/styles.clj
@@ -39,8 +39,14 @@
       ".clock-text" {:-fx-font-size "4em"
                      :-fx-font-weight "bold"}
       ".thumbnail-button:hover" (-> {:-fx-effect "innershadow(one-pass-box, rgba(0,0,0,1), 10, 1, 0, 0)"}
-                                    (as-> s (if (:cursor? cfg)
-                                              (assoc s :-fx-cursor :hand)
-                                              s)))
-      ".thumbnail-scroll-pane" {:-fx-background-color bg-color}
+                                    (as-> s (if (:cursor? cfg) (assoc s :-fx-cursor :hand) s)))
+      ".thumbnail-scroll-pane .scroll-bar" {:-fx-background-color "transparent"}
+      ".thumbnail-scroll-pane .scroll-bar > .viewport" {:-fx-background-color "transparent"}
+      ".thumbnail-scroll-pane .scroll-bar > .track" {:-fx-background-color "transparent"}
+      ".thumbnail-scroll-pane .scroll-bar > .thumb" {:-fx-background-color "#333333"}
+      ".thumbnail-scroll-pane .scroll-pane > .scroll-bar > .increment-button,
+       .thumbnail-scroll-pane .scroll-pane > .scroll-bar > .increment-button > .increment-arrow,
+       .thumbnail-scroll-pane .scroll-pane > .scroll-bar > .decrement-button,
+       .thumbnail-scroll-pane .scroll-pane > .scroll-bar > .decrement-button > .decrement-arrow" {:-fx-padding 0}
+      ".thumbnail-scroll-pane .scroll-pane .corner" {:-fx-background-color "transparent"}
       ".toggle-menu-button:hover" (if (:cursor? cfg) {:-fx-cursor :hand} {})})))
