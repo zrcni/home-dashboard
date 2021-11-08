de40e8425a401a86409c5f97e0febe058408edfb
zrcni <zrcni9@gmail.com>
2021-11-08 09:30:14 +0200

    handle events longer than one day

--- app-ts/src/main/calendar/WebCalEventFinder/WebCalEventParser.ts
+++ app-ts/src/main/calendar/WebCalEventFinder/WebCalEventParser.ts
@@ -1,5 +1,5 @@
-import ical, { ParamList } from '../../../lib/ical'
-import { isSameDay } from 'date-fns'
+import ical, { ParamList, CalendarComponent } from '../../../lib/ical'
+import { isBefore, isAfter, startOfDay, endOfDay, subDays } from 'date-fns'
 
 export class WebCalEventParser {
   private data: string
@@ -13,9 +13,27 @@ export class WebCalEventParser {
 
     return Object.values(calendar)
       .filter((obj) => obj.type === CALENDAR_COMPONENT_TYPE_VEVENT)
-      .filter((event) => isSameDay(new Date(event.start!), date))
+      .filter((event) => isEventDuring(event, date))
       .map((event) => (event.summary as ParamList).val)
   }
 }
 
 const CALENDAR_COMPONENT_TYPE_VEVENT = 'VEVENT' as const
+
+/**
+ * One day event's start and end dates seems to
+ * be the day of and the next day respectively.
+ *
+ * Using startOfDay and endOfDay might
+ * not be necessary, but lets be sure.
+ */
+function isEventDuring(event: CalendarComponent, date: Date) {
+  const start = startOfDay(new Date(event.start!))
+  // end date seems to be the next day after the event, so one day needs to be subtracted.
+  const end = endOfDay(subDays(new Date(event.end!), 1))
+  return isBetween(start, end, date)
+}
+
+function isBetween(start: Date, end: Date, date: Date) {
+  return isAfter(date, start) && isBefore(date, end)
+}
