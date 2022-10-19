7d6d4e9dfe3176a26d1df47b1acdf3f6d833769d
zrcni <zrcni9@gmail.com>
2022-10-19 21:57:42 +0300

    updated package script to copy over RPI3 sqlite3

--- app-ts/package.sh
+++ app-ts/package.sh
@@ -8,4 +8,7 @@ source ~/.nvm/nvm.sh
 # electron setup the project uses fs.rm method, which requires node >14.14 (?)
 nvm use 14.18
 
+rm -rf ./release/app/node_modules/sqlite3/*
+cp -r ./lib/rpi3-sqlite3/* ./release/app/node_modules/sqlite3
+
 npm run package
