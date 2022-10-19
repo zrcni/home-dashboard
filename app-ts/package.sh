b6407f4bd74a28c7f62426c7e1e0d305111b1e79
zrcni <zrcni9@gmail.com>
2022-10-19 22:10:29 +0300

    updated package script to only copy bindings

--- app-ts/package.sh
+++ app-ts/package.sh
@@ -8,7 +8,8 @@ source ~/.nvm/nvm.sh
 # electron setup the project uses fs.rm method, which requires node >14.14 (?)
 nvm use 14.18
 
-rm -rf ./release/app/node_modules/sqlite3/*
-cp -r ./lib/rpi3-sqlite3/* ./release/app/node_modules/sqlite3
+# Copy bindings that are built on Raspberry Pi 3 
+cp -r ./lib/rpi3-sqlite3/lib/binding/napi-v6-linux-glibc-arm \
+  ./release/app/node_modules/sqlite3/lib/binding/napi-v6-linux-glibc-arm
 
 npm run package
