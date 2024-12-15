e03a1232e56d7fc19c6500ec25ec000d405c3998
zrcni <zrcni9@gmail.com>
2024-12-15 19:23:57 +0200

    update outside conditions finder location to one that is actively providing observations

--- app-ts/src/main/conditions/OutsideConditionsFinder/OutsideConditionsFinder.ts
+++ app-ts/src/main/conditions/OutsideConditionsFinder/OutsideConditionsFinder.ts
@@ -6,7 +6,7 @@ export class OutsideConditionsFinder {
   // fmisid 101794 = Oulu, Vihreäsaari satama
   // fmisid 101799 = Oulu, Oulunsalo Pellonpää
   private static baseUrl =
-    'https://www.ilmatieteenlaitos.fi/api/weather/observations?fmisid=101799&observations=true'
+    'https://www.ilmatieteenlaitos.fi/api/weather/observations?fmisid=101794&observations=true'
 
   public static async getLatest() {
     const data = await this.getJSON(this.baseUrl)
